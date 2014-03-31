f0bb64fe6e5696e7a539136f9aab093175ec98ef
skotperez <voraginebramante@gmail.com>
2014-03-31 15:53:47 +0200

    basic header fixed navbar

--- header.php
+++ header.php
@@ -61,8 +61,9 @@ if ( is_single() || is_page() ) {
 <meta property="og:description" content="<?php echo $metadesc_fb ?>" />
 <meta property="og:url" content="<?php echo $metaperma ?>" />
 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 
 <link rel="alternate" type="application/rss+xml" title="<?php echo QUINCEM_BLOGNAME; ?> RSS Feed suscription" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="alternate" type="application/atom+xml" title="<?php echo QUINCEM_BLOGNAME; ?> Atom Feed suscription" href="<?php bloginfo('atom_url'); ?>" /> 
@@ -71,12 +72,34 @@ if ( is_single() || is_page() ) {
 <?php
 if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
 wp_head(); ?>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 
 </head>
 
 <?php // better to use body tag as the main container ?>
 <body <?php body_class(); ?>>
 <div class="container">
+
+	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#quincem-pre-navbar-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#" title="<?php echo QUINCEM_BLOGNAME; ?>"><img src="<?php echo QUINCEM_BLOGTHEME; ?>/images/quincem-logo.png" alt="<?php echo QUINCEM_BLOGNAME; ?>" /></a>
+			</div>
+			<div class="collapse navbar-collapse" id="quincem-pre-navbar-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#descubre">Descubre</a></li>
+					<li><a href="#haz">Haz</a></li>
+					<li><a href="#aprende">Aprende</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 
 <div class="row">
 	<div id="pre">
