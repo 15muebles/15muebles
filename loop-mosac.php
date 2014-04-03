f08b318a75564c99a598ef9833ff7b26d699dfa0
skotperez <voraginebramante@gmail.com>
2014-04-03 11:07:55 +0200

    icono custom meta box and output for itinerarios

--- loop-mosac.php
+++ loop-mosac.php
@@ -1,15 +1,20 @@
 <?php
 if ( $band_pts[$band_count] == 'itinerario' ) {
+	$item_tit = get_the_title();
 	$item_subtit = get_post_meta( $post->ID, '_quincem_subtit', true );
 	$item_desc = get_the_excerpt();
+	$item_icono = get_post_meta( $post->ID, '_quincem_icono',true );
+	$item_iconos_out = "<ul class='list-inline'><li><img src='" .$item_icono. "' alt='" .$item_tit. ". " .$item_subtit. "' /></li></ul>";
 
 } elseif ( $band_pts[$band_count] == 'badge' ) {
 	$item_subtit = get_post_meta( $post->ID, '_quincem_subtit', true );
 	$item_desc = "";
+	$item_iconos_out = "";
 
 } elseif ( $band_pts[$band_count] == 'actividad' ) {
 	$item_subtit = "";
 	$item_desc = "";
+	$item_iconos_out = "";
 
 }
 ?>
@@ -18,12 +23,15 @@ if ( $band_pts[$band_count] == 'itinerario' ) {
 <div <?php post_class(); ?>>
 	<?php the_post_thumbnail('thumbnail',array('class' => 'img-responsive')); ?>
 	<div class="caption">
-		<h3><?php the_title(); ?></h3>
+		<h3><?php echo $item_tit; ?></h3>
 		<?php // subtitle
 		if ( $item_subtit != '' ) { echo "<div class='mosac-item-subtit'>" .$item_subtit. "</div>"; }
 
 		// description
 		if ( $item_desc != '' ) { echo "<div class='mosac-item-desc'>" .$item_desc. "</div>"; }
+
+		// iconos
+		if ( $item_iconos_out != '' ) { echo "<div class='mosac-item-iconos'>" .$item_iconos_out. "</div>"; }
 		?>	
 	</div>
 </div>
