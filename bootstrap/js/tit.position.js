29d3c895aeb202144209f52fa21f8ac69ef1a089
skotperez <voraginebramante@gmail.com>
2014-04-04 11:28:29 +0200

    fix responsive issue when resize window to mobile width

--- bootstrap/js/tit.position.js
+++ bootstrap/js/tit.position.js
@@ -1,14 +1,17 @@
-jQuery(window).resize(function(){
+jQuery(window).on("load resize",function(){
 
-	if (jQuery(window).width() <= 767) {
-		// is mobile device; do nothing
+	if (jQuery(window).width() <= 767) { // mobile device
+		jQuery('.sec-header .sec-tit').each(function() {
+			jQuery(this).css({"width": "auto"});
+			jQuery(this).next().css({"margin-left": "0"});
+		});
+
 	} else {
-	jQuery('.sec-header .sec-tit').each(
-		function() {
+		jQuery('.sec-header .sec-tit').each(function() {
 			titWidth = jQuery(this).children('h2').width() + 10;
 			jQuery(this).css({"width": titWidth +"px"});
 			jQuery(this).next().css({"margin-left": titWidth +"px"});
-		}
-	);
+		});
+
 	}
 });
