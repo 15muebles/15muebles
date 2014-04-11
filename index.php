7e25a1b6ec494d3d0c25dcd2b24e99a3dd8289a9
skotperez <voraginebramante@gmail.com>
2014-04-11 17:36:55 +0200

    actividades partial loops in home

--- index.php
+++ index.php
@@ -61,25 +61,11 @@ foreach ( $band_pts as $band_pt ) {
 	$band_subtit = $wp_post_types[$band_pt]->labels->name;
 	$band_desc = $wp_post_types[$band_pt]->description;
 
-	// loop args
-	if ( $band_pt == 'actividad' ) {
-		$current = time();
-		$args = array(
-			'posts_per_page' => -1,
-			'post_type' => $band_pt,
-			'orderby' => 'meta_value_num',
-			'meta_key' => '_quincem_date_begin',
-			'order' => 'ASC',
-			'meta_query' => array(
-				array(
-					'key' => '_quincem_date_end',
-					'value' => $current,
-					'compare' => '>'
-				)
-			)
-		);
+	// IF ITINERARIOS OR BADGES
+	if ( $band_pt != 'actividad' ) {
 
-	} elseif ( $band_pt == 'badge' )  {
+		// loop args
+		if ( $band_pt == 'badge' )  {
 		$args = array(
 			'posts_per_page' => -1,
 			'post_type' => $band_pt,
@@ -87,18 +73,17 @@ foreach ( $band_pts as $band_pt ) {
 			'order' => 'ASC',
 		);
 
-	} elseif ( $band_pt == 'itinerario' ) {
+		} elseif ( $band_pt == 'itinerario' ) {
 		$args = array(
 			'posts_per_page' => -1,
 			'post_type' => $band_pt,
 			'orderby' => 'menu_order',
 			'order' => 'ASC',
 		);
-	}
+		}
 
-	$the_query = new WP_Query( $args );
-
-	if ( $the_query->have_posts() ) { ?>
+		$the_query = new WP_Query( $args );
+		if ( $the_query->have_posts() ) { ?>
 
 		<div id="<?php echo $band_pt; ?>" class="container-full">
 		<div class="container">
@@ -113,35 +98,138 @@ foreach ( $band_pts as $band_pt ) {
 			</div>
 			</header><!-- .sec-header .row .hair-->
 			<div class="mosac row hair">
-		<?php
-		// BEGIN *THIS* band loop
-		$tablet_count = 0;
-		$desktop_count = 0;
-		while ( $the_query->have_posts() ) : $the_query->the_post();
-			if ( $tablet_count == 3 ) { $tablet_count = 0; echo '<div class="clearfix visible-sm"></div>';  }
-			if ( $desktop_count == 5 ) { $desktop_count = 0; echo '<div class="clearfix visible-md visible-lg"></div>';  }
-			$tablet_count++; $desktop_count++;
+			<?php
+			// BEGIN *THIS* band loop
+			$tablet_count = 0;
+			$desktop_count = 0;
+			while ( $the_query->have_posts() ) : $the_query->the_post();
+				if ( $tablet_count == 3 ) { $tablet_count = 0; echo '<div class="clearfix visible-sm"></div>';  }
+				if ( $desktop_count == 5 ) { $desktop_count = 0; echo '<div class="clearfix visible-md visible-lg"></div>';  }
+				$tablet_count++; $desktop_count++;
+				include "loop-mosac.php";
 
-			include "loop-mosac.php";
-
-//			if ( $thisband_count == $band_cols[$band_count]['desktop'] ) { echo "</div>"; }
-		endwhile;
-		// END *THIS* band loop
-
-//		if ( $thisband_count != $band_cols[$band_count]['desktop'] ) { echo "</div>"; }
-		?>
+			endwhile;
+			// END *THIS* band loop
+			?>
 			</div><!-- .mosac .row .hair -->
+
 		</section>
 		</div><!-- .container -->
 		</div><!-- .container-full -->
 
-	<?php } // end if have posts
-	$band_count++;
+		<?php } // end if have posts
 
+
+	// ACTIVIDADES
+	} elseif ( $band_pt == 'actividad' ) {
+		$current = time();
+		// future actividades
+		$act_tits[] = "Próximas";
+		$act_args[] = array(
+			'posts_per_page' => -1,
+			'post_type' => $band_pt,
+			'orderby' => 'meta_value_num',
+			'meta_key' => '_quincem_date_begin',
+			'order' => 'ASC',
+			'meta_query' => array(
+				array(
+					'key' => '_quincem_date_begin',
+					'value' => $current,
+					'compare' => '>'
+				)
+			)
+		);
+		// current actividades
+		$act_tits[] = "En curso";
+		$act_args[] = array(
+			'posts_per_page' => -1,
+			'post_type' => $band_pt,
+			'orderby' => 'meta_value_num',
+			'meta_key' => '_quincem_date_begin',
+			'order' => 'ASC',
+			'meta_query' => array(
+				'relation' => 'AND',
+				array(
+					'key' => '_quincem_date_begin',
+					'value' => $current,
+					'compare' => '<'
+				),
+				array(
+					'key' => '_quincem_date_end',
+					'value' => $current,
+					'compare' => '>'
+				)
+			)
+		);
+		// past actividades
+		$act_tits[] = "Pasadas";
+		$act_args[] = array(
+			'posts_per_page' => -1,
+			'post_type' => $band_pt,
+			'orderby' => 'meta_value_num',
+			'meta_key' => '_quincem_date_begin',
+			'order' => 'ASC',
+			'meta_query' => array(
+				array(
+					'key' => '_quincem_date_end',
+					'value' => $current,
+					'compare' => '<'
+				)
+			)
+		); ?>
+
+
+		<div id="<?php echo $band_pt; ?>" class="container-full">
+		<div class="container">
+			<header class="sec-header row hair">
+			<div class="col-md-10 col-sm-10">
+				<div class="sec-tit">
+					<h2><?php echo $band_tit; ?></h2>
+					<div class="sec-subtit"><?php echo $band_subtit; ?></div>
+				</div>
+				<div class="sec-desc"><p><?php echo $band_desc; ?></p></div>
+			</div>
+			</header><!-- .sec-header .row .hair-->
+
+			<?php // actividades loops
+			$act_count = 0;
+			foreach ( $act_args as $args ) {
+
+				$the_query = new WP_Query( $args );
+				if ( $the_query->have_posts() ) { ?>
+
+				<section>
+				<div class="row">
+					<header class="col-md-10 col-sm-10">
+					<h3 class="sec-part-tit"><?php echo $act_tits[$act_count]; ?></h3>
+					</header>
+				</div><!-- .row .hair -->
+				<div class="mosac row hair">
+				<?php $tablet_count = 0;
+				$desktop_count = 0;
+				while ( $the_query->have_posts() ) : $the_query->the_post();
+					if ( $tablet_count == 3 ) { $tablet_count = 0; echo '<div class="clearfix visible-sm"></div>';  }
+					if ( $desktop_count == 5 ) { $desktop_count = 0; echo '<div class="clearfix visible-md visible-lg"></div>';  }
+					$tablet_count++; $desktop_count++;
+					include "loop-mosac.php";
+
+				endwhile; ?>
+
+				</div><!-- .mosac .row .hair -->
+				</section>
+
+			<?php 	} // end if have posts
+				$act_count++;
+			} // end actividades loop ?>
+
+		</div><!-- .container -->
+		</div><!-- .container-full -->
+
+	<?php } // END if ACTIVIDADES
+
+	$band_count++;
 }
 // END bands loop
-
 ?>
-
 
 <?php get_footer(); ?>
--- loop-mosac.php
+++ loop-mosac.php
@@ -72,7 +72,7 @@ if ( is_single() ) {
 
 ?>
 
-<article class="mosac-item aligncenter col-md-5 col-sm-3">
+<article class="mosac-item aligncenter col-md-2 col-sm-3">
 <div <?php post_class(); ?>>
 	<?php echo $item_logo; ?>
 	<div class="caption">
