00399c4e0f300f1bc67221d091d1b9692e4ceb60
skotperez <voraginebramante@gmail.com>
2014-04-20 18:55:03 -0400

    license and other credits in footer

--- footer.php
+++ footer.php
@@ -22,6 +22,11 @@ elseif ( is_page_template('about.php') ) { $epi_class = "about"; }
 		</ul>
 	</div>
 	</div><!-- .row -->
+	<div class="row explica">
+	<div class="col-md-8 col-md-offset-1">
+		<div><p><strong>El contenido de Ciudad Escuela</strong>, a menos que se indique lo contrario, está disponible para su uso bajo las condiciones de la licencia <a href="http://creativecommons.org/licenses/by-sa/4.0/deed.es_ES">Creative Commons Reconocimiento-CompartirIgual 4.0 Internacional</a>. <strong>El código de la web de Ciudad Escuela</strong> está igualmente disponible para su uso bajo las condiciones de una licencia <a href="https://github.com/skotperez/15muebles/blob/master/LICENSE">GPL2</a>, y puede <a href="https://github.com/skotperez/15muebles">descargarse libremente</a>. La web de Ciudad Escuela funciona usando <a href="http://wordpress.org">WordPress</a>.</p></div>
+	</div>
+	</div><!-- .row -->
 </footer>
 </div><!-- .container -->
 
