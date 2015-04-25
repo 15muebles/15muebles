fc83f2769535cb16a1a7d5f6d28e70d07706a664
skotperez <voraginebramante@gmail.com>
2015-04-25 13:02:12 +0200

    archive templates reorganization

--- archive.php
+++ archive.php
@@ -1,24 +1,35 @@
 <?php get_header(); ?>
 
-<?php
-if ( have_posts() ) {
-$pt = $wp_query->query_vars['post_type'];
-$pt_object = get_post_type_object( $pt );
-$tit = $pt_object->label;
-?>
-		<header>
-		<h1><?php echo $tit; ?></h1>
-		</header>
-		<div class="row">
-			<section>
-<?php while ( have_posts() ) : the_post(); ?>
-				<div>
-					<h2><?php the_title(); ?></h2>
-					<?php the_post_thumbnail($post->ID,'thumbnail',array('class' => 'img-responsive')); ?>
-					<?php the_excerpt(); ?>
-				</div>
-<?php endwhile; ?>
-			</section>
-		</div>
-<?php } ?>
+<div id="blog" class="container-full">
+<div class="container">
+	<div class="row hair">
+			<header class="col-sm-10"><h1 class="parent-tit"><?php the_archive_title(); ?></h1></header>
+	</div>
+	<div class="row">
+		<div class="col-md-7 col-sm-7">
+			<?php if ( have_posts() ) {
+				while ( have_posts() ) : the_post();
+					get_template_part('loop-blog');
+				endwhile;
+				// Previous/next page navigation.
+				the_posts_pagination( array(
+					'prev_text'          => '«',
+					'next_text'          => '»',
+					'before_page_number' => '',
+				) );
+
+			} else { echo "No hay contenido."; } ?>
+		</div><!-- .col-md-6 .col-sm-7 -->
+
+		<?php if ( is_active_sidebar( 'blog_right' ) ) : ?>
+		<aside id="blog_right" class="widget-area col-md-3 col-sm-3" role="complementary">
+			<?php dynamic_sidebar( 'blog_right' ); ?>
+		</aside><!-- #blog_right -->
+		<?php endif; ?>
+
+	</div><!-- .row -->
+
+</div><!-- .container -->
+</div><!-- .container-full -->
+
 <?php get_footer(); ?>
