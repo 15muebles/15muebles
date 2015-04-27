5edcc8bb780c40310a23154067a68a65b8e0ccb0
skotperez <voraginebramante@gmail.com>
2015-04-27 12:55:18 +0200

    link to blog in header, some style changes in blog

--- header.php
+++ header.php
@@ -159,6 +159,7 @@ $band_tits = array("Descubre","Aprende","Haz");
 					<li><a class="border-band-black" href="/about">About</a></li>
 				<?php } ?>
 					<li><a href="/preguntas-frecuentes">FAQ</a></li>
+					<li><a href="/blog">Blog</a></li>
 			</ul>
 
 			<ul id="navbar-third" class="nav navbar-nav navbar-right">
